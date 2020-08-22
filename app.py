48ae28dca6cd33e7fafe9324dcd23c2a491aeb89
jaseemckclt <jaseemckclt@gmail.com>
2020-08-22 16:45:20 +0530

    Download pic

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ import streamlit as st
 import cv2
 from PIL import Image
 import numpy as np
+import os
+import base64
 
+def get_binary_file_downloader_html(bin_file, file_label='File'):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    bin_str = base64.b64encode(data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+    return href
 
 def dodgeV2(x, y):
     return cv2.divide(x, 255 - y, scale=256)
@@ -13,9 +21,6 @@ def sketchit(img):
     img_smoothing = cv2.GaussianBlur(img_invert, (21, 21),sigmaX=0, sigmaY=0)
     final_img = dodgeV2(img_gray, img_smoothing)
     return final_img
-
-def download():
-    return f'<a href="{final.jpg}" download="Image.jpeg">Download Img</a>'
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("SketchIt")
@@ -33,4 +38,4 @@ else:
     st.write("Output Image")
     st.image(final, use_column_width=True)
     if st.button('Download Image'):
-        Image.fromarray(final).show()
+        st.markdown(get_binary_file_downloader_html(final, 'Picture'), unsafe_allow_html=True)
